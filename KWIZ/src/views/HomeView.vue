f747bea0553345dd648e56b131a266c7cf306305
Hugo MOREAU [8ff749c8-0e41-4edd-a491-4067a3d81970] <hugo.moreau@ca-ps.com>
2023-12-13 11:05:43 +0100

    frontpage-init

--- KWIZ/src/views/HomeView.vue
+++ KWIZ/src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="center-screen">
+      <h1 class="title">KWIZ</h1>
+    </div>
   </main>
 </template>
+
+<style>
+  .title {
+    font-weight: bolder;
+    font-size: 10rem;
+  }
+</style>
